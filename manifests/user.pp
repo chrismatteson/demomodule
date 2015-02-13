26053565989f1ff4ff2ef9488f307a74ee9a3e36
root <root@master.inf.puppetlabs.demo>
2015-02-13 22:37:57 +0000

    initial commit

---
+++ manifests/user.pp
@@ -0,0 +1,27 @@
+class demomodule::user (
+  $username,
+  $password,
+  ){
+  
+  user { "$username":
+    ensure     => present,
+    home       => "/home/$username",
+    managehome => true,
+    comment    => "$username account created by demomodule::user",
+    forcelocal => true,
+    groups     => 'wheel',
+    shell      => '/bin/bash',
+    password   => "$password",
+  }
+
+  file_line { "Puppet Path":
+    ensure  => present,
+    line    => 'PATH=$PATH:/opt/puppet/bin/',
+    path    => "/home/$username/.bashrc",
+    require => User["$username"],
+  }
+
+  group { 'wheel':
+    ensure => present
+  }
+}
---
+++ tests/user.pp
@@ -0,0 +1,4 @@
+class { 'demomodule::user':
+  username => 'cmatteson',
+  password => '$1$Tz0HtbMr$DfAhRUKkbLaVpwrDTwzr1.',
+}
