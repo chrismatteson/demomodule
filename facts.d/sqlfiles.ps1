0ec147b2cdb481696aac96584b9ee674cb780318
root <root@master.inf.puppetlabs.demo>
2015-04-14 05:05:45 +0000

    updates

--- facts.d/sqlfiles.ps1
+++
@@ -1,6 +0,0 @@
-$hostname = hostname
-$instances = get-service | where-object {$_.Name -like 'MSSQL$*'} | select -expand Name | %{$_ -replace 'MSSQL\$'}
-foreach ($instance in $instances) {
-  $value = sqlcmd -E -S $hostname\$instance -Q "select name, physical_name as current_file_location from sys.master_files";
-  echo "$instance => $value"
-}
--- manifests/webblog.pp
+++ manifests/webblog.pp
@@ -7,9 +7,21 @@ class demomodule::webblog {
     bindings_enable => true,
   }
   contain mysql::client 
-  package { 'wget':
-    ensure => '1.12-5.el6_6.1',
-  }
   contain wordpress
   contain demomodule::webblog::config
+
+  Class['epel'] ->
+  Class['mysql::server'] ->
+  Class['mysql::client'] ->
+  package { 'wget':
+    ensure => '1.12-5.el6_6.1',
+  } ->
+  Class['wordpress'] ->
+  Class['demomodule::webblog::config']
+
+  firewall { '100 allow http':
+    port => 80,
+    proto => tcp,
+    action => 'accept',
+  }
 }
