24115e040aea38aff6a63dfc760bcf1f4ed27798
Managed by Puppet! <root@master.inf.puppetlabs.demo>
2015-05-13 01:54:16 +0000

    added purge functionality to nagios

--- manifests/nagios_server.pp
+++ manifests/nagios_server.pp
@@ -11,6 +11,12 @@ class demomodule::nagios_server (
   include apache
   include apache::mod::php
   include apache::mod::cgi
+  file_line { 'change default':
+    ensure  => 'present',
+    path    => '/opt/puppet/lib/ruby/site_ruby/1.9.1/puppet/util/nagios_maker.rb',
+    line    => 'target = "/etc/nagios/conf.d/#{full_name.to_s}.cfg"',
+    match   => 'target = "/etc/nagios/',
+  }
   Class['epel'] ->
   package { 'nagios':
     ensure => 'present',
@@ -19,7 +25,7 @@ class demomodule::nagios_server (
     ensure => 'file',
     source => "puppet:///modules/${module_name}/commands.cfg",
     mode   => '0644',
-  }
+  } ->
   service { 'nagios':
     ensure => 'running',
   }
@@ -28,13 +34,15 @@ class demomodule::nagios_server (
   contain demomodule::nagios_common
 
   file { '/var/www/html/nagios':
-    ensure => 'link',
-    target => '/usr/share/nagios/html',
+    ensure  => 'link',
+    target  => '/usr/share/nagios/html',
+    require => Package['nagios'],
   }
 
   file { '/usr/share/nagios/html/cgi-bin':
     ensure => 'link',
     target => '/usr/lib64/nagios/cgi-bin',
+    require => Package['nagios'],
   }
 
   file { '/etc/httpd/conf.d/cgi.conf':
@@ -43,10 +51,11 @@ class demomodule::nagios_server (
   }
 
   file_line { 'disable auth':
-    ensure => 'present',
-    path   => '/etc/nagios/cgi.cfg',
-    line   => 'use_authentication=0',
-    match  => '^use_authentication=',
+    ensure  => 'present',
+    path    => '/etc/nagios/cgi.cfg',
+    line    => 'use_authentication=0',
+    match   => '^use_authentication=',
+    require => Package['nagios'],
   }
 
   Nagios_host <<| |>> {
@@ -66,6 +75,10 @@ class demomodule::nagios_server (
     mode    => '0644',
     require => Class['demomodule::nagios_common'],
     notify  => Service['nagios'],
+  }
+  resources { ['nagios_host', 'nagios_service', 'nagios_hostextinfo']:
+    purge  => 'true',
+    notify => Service['nagios'],
   }
 
   firewall { '100 allow http':
