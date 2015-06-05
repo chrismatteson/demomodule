8fd69d4bd280b2a0a7938eda409598b0d009d868
root <root@ip-10-98-10-139.us-west-2.compute.internal>
2015-06-05 04:28:29 -0400

    updates to r10k

--- manifests/r10k-setup.pp
+++ manifests/r10k-setup.pp
@@ -5,12 +5,31 @@ class demomodule::r10k-setup {
   }
 
   class { 'r10k::webhook::config':
-    enable_ssl => false,
-    protected  => false,
-    notify     => Service['webhook'],
+    protected       => false,
+    use_mcollective => false,
   }
 
   class { 'r10k::webhook':
-    require => Class['r10k::webhoot::config']
+    require   => Class['r10k::webhook::config'],
+  }
+
+#  git_webhook { 'web_post_receive_webhook':
+#    ensure       => 'present',
+#    webhook_url  => 'http://52.24.239.35:8088/payload',
+#    token        => '8ce554193cc933072433917975b67dca624ac4c0',
+#    project_name => 'chrismatteson/seteam-puppet-environments',
+#    server_url   => 'https://api.github.com/',
+#    disable_ssl_verify => true,
+#    provider     => 'github',
+#  }
+
+  git_deploy_key { 'add_deploy_key_to_puppet_control':
+    ensure       => 'present',
+    name         => $::fqdn,
+    path         => '/root/.ssh/id_dsa.pub',
+    token        => '8ce554193cc933072433917975b67dca624ac4c0',
+    server_url   => 'https://api.github.com/',
+    disable_ssl_verify => true,
+    provider     => 'github',
   }
 }
